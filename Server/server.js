77ee76b6e14599ee2c3e9d556fc4e83d420584d8
Ankit-kTiwari08 <ankitkumartiwari8360@gmail.com>
2026-04-01 20:42:13 +0530

    resolved api error

--- Server/server.js
+++ Server/server.js
@@ -9,11 +9,11 @@ dotenv.config();
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-
-app.set("trust proxy", 1);
 
 app.use(cors({
     origin: [
